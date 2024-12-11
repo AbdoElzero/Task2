7bd470580d4f71be8db7a9b224ca796113b1c6f1
AbdoElzero <127994249+AbdoElzero@users.noreply.github.com>
2024-12-11 13:05:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let layer = document.querySelector(".circle-container")
+let layerBlack = document.querySelector(".circle-container .layerblack p")
+let boxs = document.querySelector(".page .boxs")
+
+
+layerBlack.addEventListener("click", function() {
+
+  if (window.innerWidth <= 575) {
+    layer.style.display = "none"
+    boxs.style.display = "block"
+  }
+
+    boxs.style.display = "block"
+    AOS.init({
+        duration: 1000,
+        once: false,
+        disable: false,
+      });
+      AOS.refresh();
+})
